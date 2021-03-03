60e62a5c82239cebdb404f12ed74164a9fc532d6
Joseph Torres <jdtorres@ucdavis.edu>
2021-03-03 10:29:45 -0800

    one twenty five UseEffect Hook and Fetch

--- src/App.js
+++ src/App.js
@@ -50,7 +50,10 @@ function App() {
   };
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask}/>
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAddTask={showAddTask}
+      />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={onDelete} onToggle={toggleReminder} />
